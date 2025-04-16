db5d1677b89dce67951db885ad752862b8e9333d
Ali Hasan <alihasandeveloper@gmail.com>
2025-04-16 11:54:20 +0600

    intrigration with react

--- wp-ai-essential.php
+++ wp-ai-essential.php
@@ -11,14 +11,57 @@
  * Domain Path:       /languages
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit;
 }
 
 include_once 'includes/admin-menu.php';
 include_once 'includes/admin-settings.php';
 include_once 'includes/rest-api.php';
-include_once 'includes/db.php' ;
+include_once 'includes/db.php';
 
 // plugin activation hook
 register_activation_hook(__FILE__, 'wp_ai_essential_db');
+
+
+//function wp_ai_essential_add_assets()
+//{
+//    $dir = plugin_dir_url(__FILE__);
+//    wp_enqueue_style('wp-ai-essential-react-style', $dir . 'build/assets/index-BdpZjVRh.css', false, '1.0', 'all');
+//    wp_enqueue_script('wp-ai-essential-react-script', $dir . 'build/assets/index-Bs9L4ns3.js', false, '1.0', true);
+//}
+//
+//add_action('enueue_scripts', 'wp_ai_essential_add_assets');
+
+// echo '<pre>';
+// printf(plugin_dir_url( __FILE__ ));
+// echo '</pre>';
+
+function wp_ai_essential_chatbox()
+{
+    $dir = plugin_dir_url(__FILE__);
+
+    wp_enqueue_style(
+        'wp-ai-essential-react-style',
+        $dir . 'build/assets/index-BdpZjVRh.css',
+        false,
+        '1.0',
+        'all'
+    );
+
+    wp_enqueue_script(
+        'wp-ai-essential-react-script',
+        $dir . 'build/assets/index-Bs9L4ns3.js',
+        false,
+        '1.0',
+        true
+    );
+
+    ?>
+    <div id="root"></div>
+    <?php
+}
+
+add_action('wp_footer', 'wp_ai_essential_chatbox');
+
+//add_shortcode('chatbox', 'wp_ai_essential_chatbox');
